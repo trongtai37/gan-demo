39475552f790be6977da9f4aa7d98fd8da1c03f5
Tai Nguyen Trong <dongthoigian37@gmail.com>
2021-05-09 14:15:11 +0700

    Add Preview for Input Image

--- src/Components/Uploader/index.js
+++ src/Components/Uploader/index.js
@@ -3,11 +3,19 @@ import { Button, Select } from "antd";
 import Dragger from "antd/lib/upload/Dragger";
 import React from "react";
 import { ModelTypes } from "../../Constants";
+import "./styles.css";
 
 const { Option } = Select;
 
+function getBase64(img, callback) {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result));
+  reader.readAsDataURL(img);
+}
+
 const Uploader = (props) => {
   const { loading, handleUpload } = props;
+  const [inputPreview, setInputPreview] = React.useState("");
   const [inputFile, setInputFile] = React.useState([]);
   const [modelType, setModelType] = React.useState(ModelTypes.CYCLE_GAN);
 
@@ -18,21 +26,31 @@ const Uploader = (props) => {
         onRemove={() => setInputFile([])}
         beforeUpload={(file) => {
           setInputFile([file]);
+          getBase64(file, (imageUrl) => setInputPreview(imageUrl));
           return false;
         }}
         fileList={inputFile}
+        style={{
+          height: "65vh !important",
+        }}
       >
-        <p className="ant-upload-drag-icon">
-          <InboxOutlined />
-        </p>
-        <p className="ant-upload-text">
-          Click or drag file to this area to upload
-        </p>
+        {inputPreview ? (
+          <img src={inputPreview} style={{ height: "65vh" }} />
+        ) : (
+          <>
+            <p className="ant-upload-drag-icon">
+              <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">
+              Click or drag file to this area to upload
+            </p>
+          </>
+        )}
       </Dragger>
       <Select
         defaultValue={ModelTypes.CYCLE_GAN}
         onChange={(value) => setModelType(value)}
-        style={{ width: 240, marginRight: 20 }}
+        style={{ width: 240, marginRight: 20, marginTop: 20 }}
       >
         <Option value={ModelTypes.CYCLE_GAN}>Mô hình CycleGAN</Option>
         <Option value={ModelTypes.UNIT}>Mô hình UNIT</Option>
